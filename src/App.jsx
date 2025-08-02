58a79043407606356184b4ad53990c77079830f1
ferdeolazabal <ferdeolazabal@gmail.com>
2025-08-02 17:33:31 -0300

    Implement routing in App component to navigate between CommissionsScreen and BatchesScreen

--- src/App.jsx
+++ src/App.jsx
@@ -1,8 +1,15 @@
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+import BatchesScreen from './components/BatchesScreen';
 import CommissionsScreen from "./components/CommissionsScreen";
 
 function App() {
   return (
-        <CommissionsScreen />
+    <Router>
+      <Routes>
+        <Route path="/" element={<CommissionsScreen />} />
+        <Route path="/batches" element={<BatchesScreen />} />
+      </Routes>
+    </Router>
   )
 }
 
